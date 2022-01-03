16026907126dd26e8d16d762b7316de0a6a934a1
dennerm25 <78267293+dennerm25@users.noreply.github.com>
2022-01-03 16:27:19 +0100

    bug fixes for python 2.7

--- src/arxivbulletin.py
+++ src/arxivbulletin.py
@@ -191,8 +191,8 @@ class arxivbulletin:
         html += "</body></html>"
 
         # Turn these into plain/html MIMEText objects
-        part1 = MIMEText(text, "plain")
-        part2 = MIMEText(html, "html")
+        part1 = MIMEText(text.encode('utf-8'), "plain")
+        part2 = MIMEText(html.encode('utf-8'), "html")
 
         # Add HTML/plain-text parts to MIMEMultipart message
         # The email client will try to render the last part first
@@ -219,7 +219,7 @@ class arxivbulletin:
         context = ssl.create_default_context()
         server = smtplib.SMTP_SSL("smtp.gmail.com", port)
         # Login
-        server.login(self.email, pwrd)
+        server.login(self.email, self.password)
         # Send message
         server.sendmail(self.email, self.email, message.as_string())
         server.quit()
